653524080d193154dad5cafa45c38ed6d384ba7a
luisfga <luisfga@gmail.com>
2020-12-22 14:18:09 -0300

    Versão 0.1.008 -- Done -- 1. Nova tela de 'Buscar Contato' -- Done -- 2. Nova tela de 'Perfil' -- Extra -- Customização dos toasts e uso de Handler
    AddContact agora é um fragment e está inserido na navegação

--- app/src/main/java/br/com/luisfga/talkingz/TalkingzApp.java
+++ app/src/main/java/br/com/luisfga/talkingz/TalkingzApp.java
@@ -1,23 +1,26 @@
 package br.com.luisfga.talkingz;
 
-import android.app.Application;
+import android.app.*;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.ServiceConnection;
-import android.graphics.Color;
+import android.os.Build;
 import android.os.Handler;
 import android.os.IBinder;
-import android.os.Looper;
 import android.util.Log;
 import android.view.*;
 import android.widget.TextView;
 import android.widget.Toast;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 import br.com.luisfga.talkingz.database.TalkingzClientRoomDatabase;
 import br.com.luisfga.talkingz.database.entity.User;
 import br.com.luisfga.talkingz.services.messaging.MessagingService;
 import br.com.luisfga.talkingz.utils.AppDefaultExecutor;
 
+import java.util.HashSet;
+import java.util.Set;
 import java.util.UUID;
 
 /**
@@ -41,6 +44,7 @@ public class TalkingzApp extends Application  {
             startService(intent);
             Log.d(TAG, "Binding to the service from TalkingzApp.onCreate()");
             bindService(intent, serviceBoundConnection, Context.BIND_IMPORTANT);
+            createNotificationChannel();
         });
     }
 
@@ -95,6 +99,66 @@ public class TalkingzApp extends Application  {
 
         }
         Log.d(TAG, "loadUser(): User loaded? " + (mainUser != null));
+    }
+
+    /* --------------- NOTIFICATIONS HANDLING ---------------- */
+    //TODO refazer em classe separada e mais organizada
+    private Set<Integer> notificationsIDs = new HashSet<>();
+    private Set<Notification> notifications = new HashSet<>();
+    private final String NOTIFICATIONS_CHANNEL_ID = "TalkingzNotificationChannelID";
+    private final String NOTIFICATIONS_GROUP_KEY = "NOTIFICATIONS_GROUP_KEY";
+    private final int SUMMARY_ID = 0;
+    private void createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = getString(R.string.talkingzNotificationChannel);
+            String description = getString(R.string.talkingzNotificationDescription);
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(NOTIFICATIONS_CHANNEL_ID, name, importance);
+            channel.setDescription(description);
+            // Register the channel with the system; you can't change the importance
+            // or other notification behaviors after this
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+    }
+
+    private int generateNewNotificationID(){
+        int newID = notificationsIDs.size()+1;
+        notificationsIDs.add(newID);
+        return newID;
+    }
+
+    public void postNewMessageNotification(String title, String text){
+
+        if (notifications.size() > 0) {
+            text = "(+"+notifications.size()+") "+ text;
+        }
+
+        // Create an explicit intent for an Activity in your app
+        Intent intent = new Intent(this, MainActivity.class);
+//        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, 0);
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, NOTIFICATIONS_CHANNEL_ID)
+                .setSmallIcon(R.drawable.ic_new_message)
+                .setContentTitle(title)
+                .setContentText(text)
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setContentIntent(pendingIntent)
+                .setGroup(NOTIFICATIONS_GROUP_KEY)
+                .setGroupSummary(true)
+                .setAutoCancel(true);
+
+        NotificationManagerCompat manager = NotificationManagerCompat.from(this);
+        Notification notfication = builder.build();
+        notifications.add(notfication);
+        manager.notify(SUMMARY_ID, notfication);
+    }
+
+    public void clearNotifications(){
+        this.notifications = new HashSet<>();
     }
 
     /* -----------------------------------------------*/
